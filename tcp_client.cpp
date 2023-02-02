4b233c52c6574a5b3d27a5129fa2e3f062056bea
Karla Vasquez <kdvasque@usc.edu>
2023-02-02 13:11:40 -0800

    Lab 02 Commit

--- tcp_client.cpp
+++ tcp_client.cpp
@@ -14,20 +14,23 @@ int main(int argc, char const *argv[])
 { 
 	// check to see if user input is valid
 	char socket_read_buffer[1024];
-	
+
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
+
+	std::string server_ip = "kdvasque";
+	std::string server_port = "8000";
 
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
+	client_fd = socket(AF_INET, SOCK_STREAM,0);
 
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
 		return -1;
 	}
+	
 
 	// Check if the client socket was set up properly
 	if(client_fd == -1){
@@ -43,11 +46,25 @@ int main(int argc, char const *argv[])
 	hints.ai_socktype = SOCK_STREAM;
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
+	
 	// TODO: Connect() to the server (hint: you'll need to use server_addr)
-	// TODO: Retreive user input
-	// TODO: Send() the user input to the server
-	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!
-	// TODO: Close() the socket
 
+
+
+	connect(client_fd,server_addr->ai_addr,server_addr->ai_addrlen);
+	// TODO: Retreive user input
+	std::string theInput = "";
+	std::cout << "Please enter a message: ";
+	std::getline(std::cin, theInput);
+
+	// TODO: Send() the user input to the server
+	send(client_fd, theInput.c_str(), theInput.length(), 0);
+
+	printf("Sent");
+	// TODO: Recieve any messages from the server and print it here. Don't forget to make sure the string is null terminated!;
+	recv(client_fd, socket_read_buffer, 1024, 0);
+	printf("%s\n",socket_read_buffer);
+	// TODO: Close() the socket
+	close(client_fd);
 	return 0; 
 } 
